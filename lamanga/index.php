5a315b7532dcc4b1cdb9b1804267716e6c8933ef
adieush <adieush@yahoo.com>
2012-12-03 02:28:57 +0200

    New

---
+++ lamanga/index.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+  <meta name="keywords" content="" />
+  <meta name="description" content="" />
+  <link href="/lamanga/css/style.css" rel="stylesheet" media="all" />
+  <link href="/lamanga/css/menu_assets/styles.css" rel="stylesheet" type="text/css">
+  <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.5/jquery.min.js "></script>
+  <script src="/lamanga/js/myjs.js "></script>
+
+  <title>La Manga</title>
+</head>
+<body>
+  <div class="wrap">
+    <header>
+      <div class="lamanga-logo"></div>
+    </header>
+    <div id='cssmenu'>
+      <ul>
+        <li class='active '><a href='#'><span>Главная</span></a></li>
+        <li><a href='#'><span>La Manga</span></a></li>
+        <li><a href='#'><span>Развлечения</span></a></li>
+        <li><a href='#'><span>Апартаменты</span></a></li>
+        <li><a href='#'><span>Прокат Авто</span></a></li>
+        <li><a href='#'><span>Партнеры</span></a></li>
+        <li><a href='#'><span>Контакты</span></a></li>
+      </ul>
+    </div>
+    <div class="left-sidebar">1</div>
+    <div class="center-content">
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin dapibus tristique accumsan. Donec tincidunt mauris non lacus fermentum ornare.
+        In eu erat ac tortor ornare aliquam quis et neque. Mauris a libero id urna interdum ultricies. 
+        Phasellus diam nulla, vehicula eget posuere sit amet, tempor nec neque. Quisque et metus tellus, a pulvinar mi. 
+        Nullam enim arcu, porttitor vitae pulvinar eget, imperdiet ac nibh. Praesent sit amet dictum arcu. 
+        Duis sodales lectus vitae eros eleifend in imperdiet tortor accumsan. Pellentesque sit amet suscipit dui. Donec sodales adipiscing justo non porta. 
+        Donec tincidunt augue et velit dignissim eget suscipit erat convallis. Curabitur vitae purus enim, vitae tincidunt odio.</p>
+      <p>Donec eros turpis, molestie a vulputate nec, scelerisque et leo. 
+        Quisque a leo vel elit pulvinar commodo. Maecenas ut nisl eget dui lobortis tristique et sed nisl. 
+        Curabitur elementum sodales semper. Sed in elit felis, vel laoreet felis. 
+        Suspendisse consectetur consequat dolor sit amet cursus. 
+        Pellentesque ac ipsum elit. Phasellus elementum sodales dui eu elementum.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin dapibus tristique accumsan. Donec tincidunt mauris non lacus fermentum ornare.
+        In eu erat ac tortor ornare aliquam quis et neque. Mauris a libero id urna interdum ultricies. 
+        Phasellus diam nulla, vehicula eget posuere sit amet, tempor nec neque. Quisque et metus tellus, a pulvinar mi. 
+        Nullam enim arcu, porttitor vitae pulvinar eget, imperdiet ac nibh. Praesent sit amet dictum arcu. 
+        Duis sodales lectus vitae eros eleifend in imperdiet tortor accumsan. Pellentesque sit amet suscipit dui. Donec sodales adipiscing justo non porta. 
+        Donec tincidunt augue et velit dignissim eget suscipit erat convallis. Curabitur vitae purus enim, vitae tincidunt odio.</p>
+      <p>Donec eros turpis, molestie a vulputate nec, scelerisque et leo. 
+        Quisque a leo vel elit pulvinar commodo. Maecenas ut nisl eget dui lobortis tristique et sed nisl. 
+        Curabitur elementum sodales semper. Sed in elit felis, vel laoreet felis. 
+        Suspendisse consectetur consequat dolor sit amet cursus. 
+        Pellentesque ac ipsum elit. Phasellus elementum sodales dui eu elementum.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin dapibus tristique accumsan. Donec tincidunt mauris non lacus fermentum ornare.
+        In eu erat ac tortor ornare aliquam quis et neque. Mauris a libero id urna interdum ultricies. 
+        Phasellus diam nulla, vehicula eget posuere sit amet, tempor nec neque. Quisque et metus tellus, a pulvinar mi. 
+        Nullam enim arcu, porttitor vitae pulvinar eget, imperdiet ac nibh. Praesent sit amet dictum arcu. 
+        Duis sodales lectus vitae eros eleifend in imperdiet tortor accumsan. Pellentesque sit amet suscipit dui. Donec sodales adipiscing justo non porta. 
+        Donec tincidunt augue et velit dignissim eget suscipit erat convallis. Curabitur vitae purus enim, vitae tincidunt odio.</p>
+      <p>Donec eros turpis, molestie a vulputate nec, scelerisque et leo. 
+        Quisque a leo vel elit pulvinar commodo. Maecenas ut nisl eget dui lobortis tristique et sed nisl. 
+        Curabitur elementum sodales semper. Sed in elit felis, vel laoreet felis. 
+        Suspendisse consectetur consequat dolor sit amet cursus. 
+        Pellentesque ac ipsum elit. Phasellus elementum sodales dui eu elementum.</p>
+    </div>
+    <div class="right-sidebar">
+      <a href="#">
+        <div class="lamanga-map">
+          <h4>La Manga</h4>
+          <div class="lamanga-map-icon"></div>
+        </div>
+      </a>
+    </div>
+    <div class="empty"></div>
+  </div>
+  <div class="footer">
+    <p>&copy; All other marks are the property of their respective owners.</p>
+  </div>
+</body>
+</html>
